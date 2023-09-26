a660d0ca7774ba5310afc5f3cc7e1db4aeb6971e
c2016105 <S.Mottur-Venugopal2@ncl.ac.uk>
2023-09-26 18:37:17 +0100

    Add Practical1 completed code for blog

--- Practical1/app.py
+++ Practical1/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from main.views import main_blueprint
+from blog.views import blog_blueprint
+from users.views import users_blueprint
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+app.register_blueprint(blog_blueprint)
+app.register_blueprint(users_blueprint)
+app.register_blueprint(main_blueprint)
 
 if __name__ == '__main__':
     app.run()
