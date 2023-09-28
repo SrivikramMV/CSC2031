0f78a486a91a3a1d8c41230602f6ba3908c571c9
c2016105 <S.Mottur-Venugopal2@ncl.ac.uk>
2023-09-28 12:54:13 +0100

    Docs Re-organized the practical 1 files into 1 folder

--- Practical1/app.py
+++ Practical1/app.py
@@ -3,6 +3,7 @@ from main.views import main_blueprint
 from blog.views import blog_blueprint
 from users.views import users_blueprint
 
+
 app = Flask(__name__)
 app.register_blueprint(blog_blueprint)
 app.register_blueprint(users_blueprint)
