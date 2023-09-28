9412fecf766656b675410b323ec01d58e3494321
SrivikramMV <S.Mottur-Venugopal2@ncl.ac.uk>
2023-09-29 00:10:21 +0100

    add Practical 2 Part 1

--- Practical2/app.py
+++ Practical2/app.py
@@ -1,11 +1,18 @@
+import secrets
+
 from flask import Flask
+from main.views import main_blueprint
+from blog.views import blog_blueprint
+from users.views import users_blueprint
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.register_blueprint(blog_blueprint)
+app.register_blueprint(users_blueprint)
+app.register_blueprint(main_blueprint)
 
 
 if __name__ == '__main__':
