d1afd74bc2ce47eeda30a445ee58be9fe086861b
SrivikramMV <S.Mottur-Venugopal2@ncl.ac.uk>
2023-09-29 02:10:16 +0100

    add Practical 2 Part 3

--- Practical2/app.py
+++ Practical2/app.py
@@ -1,6 +1,6 @@
 import secrets
 
-from flask import Flask
+from flask import Flask, render_template
 from main.views import main_blueprint
 from blog.views import blog_blueprint
 from users.views import users_blueprint
@@ -14,6 +14,17 @@ app.register_blueprint(blog_blueprint)
 app.register_blueprint(users_blueprint)
 app.register_blueprint(main_blueprint)
 
+@app.errorhandler(403)
+def internal_error(error):
+    return render_template('403.html'), 500
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 500
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
--- Practical2/blog/views.py
+++ Practical2/blog/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 
 blog_blueprint = Blueprint('blog', __name__, template_folder='templates')
 
@@ -6,3 +6,4 @@ blog_blueprint = Blueprint('blog', __name__, template_folder='templates')
 @blog_blueprint.route('/')
 def blog():
     return render_template('blog/blog.html')
+    # abort
